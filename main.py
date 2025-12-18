ef62b83977327a706afeddd93f52ad0a081672b7
Colin <leocolin7002@gmail.com>
2025-12-18 16:09:50 +0100

    ajout de l'url et de l'id

--- main.py
+++ main.py
@@ -19,29 +19,28 @@ def orchestrate_matching(cv_path: str):
         return
 
     # 3. Lancement du matching sémantique avec filtres optionnels
-    # Vous pouvez modifier les filtres (location, contract_type) directement ici pour vos tests
+    # Possibilité de filtrer par lieu, type de contrat, expérience, etc.
     logger.info("Recherche des meilleures offres en base...")
     results = matcher.find_matches(
         profile_text=cv_text,
         top_n=5,
-        location="Paris",        # Ex: "Paris"
-        contract_type="CDI",    # Ex: "CDI"
+        location="Paris",
+        contract_type="CDI",
         experience="D"
     )
 
-    # 4. Affichage simple des résultats
-    print("\n" + "="*60)
     print(f"TOP MATCHES POUR : {cv_path}")
-    print("="*60)
 
     if not results:
         print("Aucun résultat trouvé.")
+
     else:
         for i, res in enumerate(results, 1):
             score = round(res.similarity_score * 100, 2)
             print(f"{i}. [{score}%] {res.title}")
             print(f"   🏢 {res.company} | 📍 {res.location}")
             print(f"   📜 {res.type_contrat} | ⏳ {res.experience_exigee}")
+            print(f"   🔗 Lien : {res.url} | ID: {res.id}")
 
 if __name__ == "__main__":  
     orchestrate_matching("data/CVs/f923c0e9e6a7ca1d2cb3c0e78a868dac.pdf") 
